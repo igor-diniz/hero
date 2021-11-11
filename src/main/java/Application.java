57707e269639b4ac3d23527ea96a8f850c18f125
up202000162 <up202000162@fe.up.pt>
2021-11-11 23:57:05 +0000

    after Lanterna

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,27 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import java.io.IOException;
+
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+
+        TerminalSize terminalSize = new TerminalSize(40,20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        terminal = terminalFactory.createTerminal();
+
+        screen.clear();
+        screen.setCharacter(10,10, TextCharacter.DEFAULT_CHARACTER);
+        screen.refresh();
     }
 }
